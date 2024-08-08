b622572e8d4a466c8ab70563e7e5466032b5b82b
Oliver Kwok <106655107+OliverKwok@users.noreply.github.com>
2024-08-08 17:39:36 +0100

    update name of variable

--- excel.ts
+++ excel.ts
@@ -1,6 +1,13 @@
 import * as XLSX from "xlsx";
 const db = require("./db/db.js");
-const excelFileName = "abc";
+
+const today = new Date();
+const year = today.getFullYear();
+const month = String(today.getMonth() + 1).padStart(2, "0");
+const day = String(today.getDate()).padStart(2, "0");
+
+const wipExcelFileName = `T185736_Honor_WIP_${year}${month}${day}`;
+const UnclosedOrderExcelFileName = `线下未闭环工单_原始明细全量导出 ${year}${month}${day}`;
 const wipSheetName = "WIP";
 
 interface Wip {
@@ -9,14 +16,28 @@ interface Wip {
   status: string;
 }
 
-async function importExcel(sheetName: string) {
+interface WorkOrder {
+  work_order_id: string;
+  work_order_number: string;
+  status: string;
+  sn: string;
+  product_series: string;
+  model_name: string;
+  internal_name: string;
+  order_created_date: string;
+  wip_status: string;
+  comment: string;
+  customer_name: string;
+}
+
+async function importExcel(excelFileName: string, sheetName: string) {
   const workbook = XLSX.readFile(`./import/${excelFileName}.xlsx`);
   const worksheet = workbook.Sheets[sheetName];
   const jsonData = XLSX.utils.sheet_to_json(worksheet);
   return jsonData;
 }
 
-async function writeWorkOrder(input: Wip[]) {
+async function wipWriteToDb(input: Wip[]) {
   const transaction = await db.transaction();
   try {
     for (const item of input) {
@@ -41,18 +62,23 @@ async function writeWorkOrder(input: Wip[]) {
 
 async function main() {
   try {
-    const importExcelResult = await importExcel(wipSheetName);
-    const importExcelResultTransform = importExcelResult.map((item: any) => {
-      return {
-        job_number: item["Job Number"],
-        sn: item["Serial In"],
-        status: item["Status"],
-      };
-    });
-    const writeWorkOrderResult = await writeWorkOrder(
-      importExcelResultTransform
+    const wipImportExcelResult = await importExcel(
+      wipExcelFileName,
+      wipSheetName
     );
-    console.log(`Inserted/Updated ${writeWorkOrderResult} rows`);
+    const wipImportExcelResultTransform = wipImportExcelResult.map(
+      (item: any) => {
+        return {
+          job_number: item["Job Number"],
+          sn: item["Serial In"],
+          status: item["Status"],
+        };
+      }
+    );
+    const writeWorkOrderResult = await wipWriteToDb(
+      wipImportExcelResultTransform
+    );
+    console.log(`Inserted/Updated ${writeWorkOrderResult} rows for WIP`);
   } catch (error) {
     console.error("Error processing work orders:", error);
   } finally {
